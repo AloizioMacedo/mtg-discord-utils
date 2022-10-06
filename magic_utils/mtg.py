747f3779a1fef0a99a26fbc214e107f9b2513dc5
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-10-06 18:09:57 -0300

    Add commands to create and search deck

--- magic_utils/mtg.py
+++ magic_utils/mtg.py
@@ -6,6 +6,10 @@ from typing import Callable, Union, cast
 
 import aiohttp
 import discord
+from sqlalchemy.orm import Session
+
+from binary_search import search_for_cards
+from model import User, engine
 
 SCRYFALL_URL = "https://api.scryfall.com"
 
@@ -15,6 +19,8 @@ class ValidCommandName(Enum):
     get_rulings = "get_rulings"
     commands = "commands"
     get_card = "get_card"
+    create_deck = "create_deck"
+    search_deck = "search_deck"
 
 
 # forest, island, mountain, plains, swamp
@@ -210,6 +216,95 @@ class ListCommands(CommandStrategy):
         ]
 
 
+class CreateDeck(CommandStrategy):
+    command = ValidCommandName.create_deck
+
+    async def show_help(self, message: discord.Message) -> None:
+        await message.channel.send(
+            _build_help(self) + "\n{MTG_ARENA_DECK_LIST}"
+        )
+
+    @command_with_help
+    async def process_command(
+        self, message: discord.Message, rest_of_command: str
+    ) -> Union[list[CardInfo], list[str]]:
+        potential_card_names = [
+            x.lower().strip() for x in rest_of_command.split("\n")
+        ]
+        cards_with_quantities = [
+            x.split(maxsplit=1) for x in potential_card_names
+        ]
+        card_names: list[str] = []
+        for card_with_quantity in cards_with_quantities:
+            try:
+                int(card_with_quantity[0])
+                card_names.append(card_with_quantity[1])
+            except:
+                continue
+
+        cards = search_for_cards(card_names)
+        author = message.author
+
+        with Session(engine) as session:
+            query = session.query(User).filter(User.discord_id == author.id)
+            result = query.first()
+
+            if result:
+                result = cast(User, result)
+                result.deck = {"deck": cards}  # type: ignore
+
+            else:
+                new_user = User(
+                    discord_id=author.id,
+                    name=author.name,
+                    deck={"deck": cards},
+                )
+                session.add(new_user)
+
+            session.commit()
+
+        return [f"Deck with {len(card_names)} succesfully uploaded."]
+
+
+class SearchDeck(CommandStrategy):
+    command = ValidCommandName.search_deck
+
+    async def show_help(self, message: discord.Message) -> None:
+        await message.channel.send(_build_help(self) + "{keyword}")
+
+    @command_with_help
+    async def process_command(
+        self, message: discord.Message, rest_of_command: str
+    ) -> Union[list[CardInfo], list[str]]:
+        author = message.author
+        text_query = rest_of_command.lower()
+
+        with Session(engine) as session:
+            query = session.query(User).filter(User.discord_id == author.id)
+            result = query.first()
+
+            if result:
+                result = cast(User, result)
+
+            else:
+                return ["Could not find a deck."]
+
+            cards: list[dict] = result.deck["deck"]  # type: ignore
+
+        query_results = [
+            card for card in cards if text_query in card["oracle_text"].lower()
+        ]
+
+        return [
+            CardInfo(
+                card["name"],
+                card["image_uris"]["small"],
+                card["image_uris"]["normal"],
+            )
+            for card in query_results
+        ]
+
+
 async def _get_dual(
     types: tuple[str, str], session: aiohttp.ClientSession
 ) -> CardInfo:
@@ -236,4 +331,6 @@ COMMANDS: dict[ValidCommandName, CommandStrategy] = {
     ValidCommandName.get_rulings: GetRulings(),
     ValidCommandName.commands: ListCommands(),
     ValidCommandName.get_card: GetCard(),
+    ValidCommandName.create_deck: CreateDeck(),
+    ValidCommandName.search_deck: SearchDeck(),
 }
