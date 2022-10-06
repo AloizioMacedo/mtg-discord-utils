b2c38f9219536949b5eb6f5a6c603b5ff752201a
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-10-06 20:10:00 -0300

    bugfix: Change way to search for cards
    Initialize a hashmap instead of making a binary search.
    This was decided since a binary search on text with
    a lot of diacritics and other issues can result in unexpected
    situations.

--- magic_utils/binary_search.py
+++ magic_utils/binary_search.py
@@ -1,5 +1,5 @@
 import json
-from bisect import bisect_left
+import unicodedata
 from pathlib import Path
 
 PARENT = Path(__file__).parent
@@ -7,20 +7,32 @@ PARENT = Path(__file__).parent
 SORTED_CARDS_DB = PARENT.joinpath("sorted-cards.json")
 
 
+def _treat_string(s: str) -> str:
+    return _strip_accents(s.split("//")[0].strip().lower())
+
+
+def _strip_accents(s: str) -> str:
+    return "".join(
+        c
+        for c in unicodedata.normalize("NFD", s)
+        if unicodedata.category(c) != "Mn"
+    )
+
+
+with open(SORTED_CARDS_DB) as file:
+    all_cards: list[dict[str, str]] = json.load(file)
+    CARD_HASHMAP = {_treat_string(card["name"]): card for card in all_cards}
+
+
 def search_for_cards(names: list[str]) -> list[dict]:
     cards_list: list[dict] = []
-    names = [name.strip().lower() for name in names]
+    treated_names = [_treat_string(name) for name in names]
 
-    with open(SORTED_CARDS_DB) as file:
-        all_cards: list[dict[str, str]] = json.load(file)
-        all_cards_names = [card["name"].strip().lower() for card in all_cards]
-        for name in names:
-            card: dict[str, str] = all_cards[
-                bisect_left(all_cards_names, name)
-            ]
-            if card["name"].strip().lower() != name:
-                raise ValueError("Weird name.")
+    for name in treated_names:
+        card: dict[str, str] = CARD_HASHMAP[name]
+        if _treat_string(card["name"]) != name:
+            raise ValueError(f"Weird name: {card['name']}")
 
-            cards_list.append(card)
+        cards_list.append(card)
 
     return cards_list
