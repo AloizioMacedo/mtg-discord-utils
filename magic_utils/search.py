2943efb02145674dffdf90299e9e864f7d83f023
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-10-08 14:27:38 -0300

    Encapsulate acquirement of cards

--- magic_utils/search.py
+++ magic_utils/search.py
@@ -9,23 +9,28 @@ CARDS_1_DB = PARENT.joinpath("oracle-cards-1.json")
 CARDS_2_DB = PARENT.joinpath("oracle-cards-2.json")
 CARDS_3_DB = PARENT.joinpath("oracle-cards-3.json")
 
-all_cards: list[dict[str, str]] = []
 
-with open(CARDS_1_DB) as file:
-    all_cards += json.load(file)
+def _get_cards_hashmap():
+    all_cards: list[dict[str, str]] = []
 
-with open(CARDS_2_DB) as file:
-    all_cards += json.load(file)
+    with open(CARDS_1_DB) as file:
+        all_cards += json.load(file)
 
-with open(CARDS_3_DB) as file:
-    all_cards += json.load(file)
+    with open(CARDS_2_DB) as file:
+        all_cards += json.load(file)
 
-KEYS = ["name", "image_uris", "oracle_text"]
+    with open(CARDS_3_DB) as file:
+        all_cards += json.load(file)
 
-CARD_HASHMAP = {
-    treat_string(card["name"]): {key: card.get(key, "") for key in KEYS}
-    for card in all_cards
-}
+    keys = ["name", "image_uris", "oracle_text"]
+
+    return {
+        treat_string(card["name"]): {key: card.get(key, "") for key in keys}
+        for card in all_cards
+    }
+
+
+CARD_HASHMAP = _get_cards_hashmap()
 
 
 def search_for_cards(names: list[str]) -> list[dict]:
