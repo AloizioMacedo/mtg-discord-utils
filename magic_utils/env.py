0c8be158bd767aef6e143b57185821db66d31971
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-10-08 11:58:07 -0300

    Allow TOKEN to be an environment variable

--- magic_utils/env.py
+++ magic_utils/env.py
@@ -1,4 +1,9 @@
 import os
 
+try:
+    from _env import TOKEN
+except ModuleNotFoundError:
+    TOKEN = os.environ.get("TOKEN")
+
 POSTGRES_SERVICE = os.environ.get("POSTGRES_SERVICE", "localhost:15444")
 FUZZY_THRESHOLD = float(os.environ.get("FUZZY_THRESHOLD", 85))
