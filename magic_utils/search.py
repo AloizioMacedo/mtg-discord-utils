90cb6665d27df785e8fb1d7cc7ba40fd81fb9ca1
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-10-09 11:21:11 -0300

    Remove deprecated code in search.py

--- magic_utils/search.py
+++ magic_utils/search.py
@@ -1,40 +1,9 @@
-import json
-from pathlib import Path
 from typing import cast
 
 from sqlalchemy.orm import Session
 
 from model import Card, engine
 from string_treatment import treat_string
-
-PARENT = Path(__file__).parent
-
-CARDS_1_DB = PARENT.joinpath("oracle-cards-1.json")
-CARDS_2_DB = PARENT.joinpath("oracle-cards-2.json")
-CARDS_3_DB = PARENT.joinpath("oracle-cards-3.json")
-
-
-def _get_cards_hashmap():
-    all_cards: list[dict[str, str]] = []
-
-    with open(CARDS_1_DB) as file:
-        all_cards += json.load(file)
-
-    with open(CARDS_2_DB) as file:
-        all_cards += json.load(file)
-
-    with open(CARDS_3_DB) as file:
-        all_cards += json.load(file)
-
-    keys = ["name", "image_uris", "oracle_text"]
-
-    return {
-        treat_string(card["name"]): {key: card.get(key, "") for key in keys}
-        for card in all_cards
-    }
-
-
-CARD_HASHMAP = _get_cards_hashmap()
 
 
 def search_for_card_ids(names: list[str]) -> list[int]:
