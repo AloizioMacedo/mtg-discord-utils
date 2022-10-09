132f3bde2cc6d6c4c9fca1be61256a8cb14c8b95
Aloizio Macedo <aloiziomacedo@gmail.com>
2022-10-09 15:59:30 -0300

    Create new models
    The main new feature is a table of decks, allowing players to create
    up to 3 decks.

--- magic_utils/model.py
+++ magic_utils/model.py
@@ -1,5 +1,14 @@
-from sqlalchemy import JSON, BigInteger, Column, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import (
+    JSON,
+    BigInteger,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    create_engine,
+)
+from sqlalchemy.orm import declarative_base, relationship
 
 from env import POSTGRES_CONNECTION_URL
 
@@ -14,7 +23,11 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     discord_id = Column(BigInteger)
     name = Column(String)
-    deck = Column(JSON)
+    selected_deck_id = Column(Integer)
+
+    decks = relationship(
+        "Deck", back_populates="user", cascade="all, delete-orphan"
+    )
 
 
 class Card(Base):
@@ -23,6 +36,18 @@ class Card(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     card_attrs = Column(JSON)
+
+
+class Deck(Base):
+    __tablename__ = "deck"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    name = Column(String)
+    deck = Column(JSON)
+    timestamp = Column(BigInteger)
+
+    user = relationship("User", back_populates="decks")
 
 
 Base.metadata.create_all(engine)
